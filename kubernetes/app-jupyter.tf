f2731174fc2655a9338c6c2fcb3a11edf909eeed
Ming Fang <mingfang@mac.com>
2025-03-22 15:17:24 +0000

    wait for python

--- kubernetes/app-jupyter.tf
+++ kubernetes/app-jupyter.tf
@@ -13,12 +13,10 @@ resource "coder_script" "jupyter" {
   display_name = "jupyter"
   run_on_start = true
   script       = <<-EOF
-  # if python is not installed...
-  if [ $(pyenv global) == "system" ]; then
-    echo "Installing Python 3.11..."
-    pyenv install 3.11
-    pyenv global 3.11
-  fi
+  until [ -e /tmp/coder_script.python ]; do
+    echo "Waiting for /tmp/coder_script.python..."
+    sleep 3
+  done
 
   echo "Installing Jupyter..."
   pip install -U jupyterlab
@@ -26,12 +24,14 @@ resource "coder_script" "jupyter" {
   echo "👷 Starting Jupyter..."
   SHELL=/bin/bash jupyter-lab --NotebookApp.ip='*' --no-browser --ServerApp.token='' --ServerApp.password='' > /tmp/jupyter.log 2>&1 &
   EOF
+
+  depends_on = [coder_script.python]
 }
 
-resource "coder_app" "jupyterlab" {
+resource "coder_app" "jupyter" {
   count        = data.coder_parameter.jupyter.value ? data.coder_workspace.me.start_count : 0
   agent_id     = coder_agent.pod.id
-  slug         = "jupyterlab"
+  slug         = "jupyter"
   display_name = "JupyterLab"
   url          = "http://localhost:8888"
   icon         = "/icon/jupyter.svg"
