f2ae6322a03e6090fe9a1103b0a6f5e9f38b77b5
Ming Fang <mingfang@mac.com>
2025-08-25 20:21:52 +0000

    upgrade to v1.4.1

--- kubernetes/app-vscode-web.tf
+++ kubernetes/app-vscode-web.tf
@@ -10,7 +10,8 @@ data "coder_parameter" "vscode" {
 module "vscode-web" {
   count   = data.coder_parameter.vscode.value ? data.coder_workspace.me.start_count : 0
   source  = "registry.coder.com/modules/vscode-web/coder"
-  version = "1.1.0"
+  version = "1.4.1"
+
   #   extensions     = ["github.copilot", "ms-python.python"]
   accept_license = true
   agent_id       = coder_agent.pod.id
